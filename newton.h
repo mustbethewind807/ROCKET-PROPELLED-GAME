d0eec32c9655d1b6f3be5e6ebe48bdc67e2cb832
mustbethewind807 <ryanzzhu69420@gmail.com>
2025-11-19 21:57:18 -0500

    Added newton.h header file

---
+++ newton.h
@@ -0,0 +1,10 @@
+#ifndef NEWTON_H
+#define NEWTON_H
+
+float* vectorCreate(float, float);
+float* vectorAdd(float*, float*);
+float* vectorSub(float*, float*);
+float* vectorMult(float*, float);
+float* vectorDiv(float*, float);
+
+#endif /*NEWTON_H*/
