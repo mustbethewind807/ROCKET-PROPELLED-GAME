d754a1f75afb6c7e063b58c465f3d4ca57a44299
mustbethewind807 <ryanzzhu69420@gmail.com>
2025-12-02 22:15:10 -0500

    added window

--- ROCKETGAME.cpp
+++ ROCKETGAME.cpp
@@ -19,6 +19,9 @@ void error_callback(int error, const char* description) {
     std::cerr << "GLFW Error " << error << ": " << description << "\n";
 }
 
+// other functions
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); // window resize callback
+
 int main()
 {
   // Example usage of std::vector to ensure C++23 features are supported
@@ -37,10 +40,13 @@ int main()
     return 1;
   }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  // setup
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   GLFWwindow* window = glfwCreateWindow(800, 600, "ROCKET PROPELLED GAME", NULL, NULL);
+
+  // does exactly what you think
   if (window == NULL)
   {
     std::cout << "Failed to create GLFW window\n";
@@ -49,13 +55,30 @@ int main()
   }
   glfwMakeContextCurrent(window);
 
+  // does exactly what you think
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
     std::cout << "Failed to initialize GLAD\n";
     return 1;
   }
 
+  // viewport and window stuff
   glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // main loop
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // exit loop
+  glfwTerminate();
   return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}  
