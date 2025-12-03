6fd20dc592c20ce3187e889be9c9fde2ef081f4f
mustbethewind807 <ryanzzhu69420@gmail.com>
2025-12-02 21:32:39 -0500

    Added testing stuff

--- ROCKETGAME.cpp
+++ ROCKETGAME.cpp
@@ -1,12 +1,40 @@
-#include "newton.h"
+// ROCKETGAME.cpp: Contains the main function and most gameplay stuff
+
+// newton.hpp is my crappy game engine
+#include "newton.hpp"
+
+// standard libraries (i use the old c libraries because i forget std:: ones exist)
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <string>
 #include <vector>
-#include <GL/glew.h>
+
+// glad and glfw for graphics
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
+  // Example usage of std::vector to ensure C++23 features are supported
   std::vector<float> a {5, 5};
+
+  // example output to verify program runs
+  std::cout << "Hello, Rocket Game!\n";
+
+  // make window
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "ROCKET PROPELLED GAME", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
   return 0;
 }
